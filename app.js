f0ba2457d6d1396dd281d8d7721c6981446a322e
Danilo Scipioni <daniloedu@gmail.com>
2022-02-19 14:40:58 -0300

    Swagger configuration

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const openapi = require('openapi-comment-parser');
+const swaggerUi = require('swagger-ui-express');
 
+const spec = openapi();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(spec));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
