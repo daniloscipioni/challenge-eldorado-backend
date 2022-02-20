f1c5b7c38fee879d69ae157ce8ea61ffb73b2a85
Danilo Scipioni <daniloedu@gmail.com>
2022-02-20 19:44:21 -0300

    Device delete and register developed

--- src/controllers/device.controller.js
+++ src/controllers/device.controller.js
@@ -30,18 +30,43 @@ exports.listAllDevices = async (req, res) => {
 
 exports.registerDevice = async (req, res) => {
     try {
-        
+
+        db.query(`INSERT INTO tbl_device(id_category, color, part_number)VALUES ('${req.category}','${req.color}','${req.partNumber}')`, function (err, result, fields) {
+            if (err) throw err;
+
+        });
+        return {
+            data: `Device  ${req.id_category} added!`,
+            success: true
+        }
     } catch (error) {
-        
+        return {
+            data: 'Error',
+            success: false,
+            rowCount: 0,
+            message: 'Device not registered!',
+        };
     }
 
 }
 
 exports.deleteDevice = async (req, res) => {
     try {
-        
+        const response = await db.query(`DELETE FROM tbl_device where id = ${req}`, function (err, result, fields) {
+            if (err) console.log(err.sqlMessage);
+        });
+        return {
+            data: "Device must be removed",
+
+        };
     } catch (error) {
-        
+        return {
+            data: error,
+            success: false,
+            rowCount: 0,
+
+        };
     }
+
 
 }
--- src/routes/device.routes.js
+++ src/routes/device.routes.js
@@ -3,4 +3,15 @@ const deviceController = require('../controllers/device.controller');
 
 router.get('/devices', deviceController.listAllDevices);
 
+router.post('/add-device', async(req, res)=> {
+    const register = await deviceController.registerDevice(req.body);
+    res.status(201).json(register);
+})
+
+router.delete('/remove-device/:id', async(req, res)=> {
+    const deleteRegister = await deviceController.deleteDevice(req.params.id);
+    res.status(200).json(deleteRegister);
+})
+
+
 module.exports = router;
