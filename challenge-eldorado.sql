6ab29491cc7413f6a611a317bda276bdd787cf75
Danilo Scipioni <daniloedu@gmail.com>
2022-02-20 20:15:13 -0300

    Fixing database connection

--- challenge-eldorado.sql
+++ challenge-eldorado.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `heroku_ecd2cdd291b0ba0`.`tbl_category` (
+CREATE TABLE `heroku_dc094d6283f186e`.`tbl_category` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE `heroku_ecd2cdd291b0ba0`.`tbl_device` (
+CREATE TABLE `heroku_dc094d6283f186e`.`tbl_device` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `id_category` INT NULL,
   `color` VARCHAR(16) NULL,
@@ -12,6 +12,6 @@ CREATE TABLE `heroku_ecd2cdd291b0ba0`.`tbl_device` (
   INDEX `FK_CATEGORIA_idx` (`id_category` ASC),
   CONSTRAINT `FK_CATEGORIA`
     FOREIGN KEY (`id_category`)
-    REFERENCES `heroku_ecd2cdd291b0ba0`.`tbl_category` (`id`)
+    REFERENCES `heroku_dc094d6283f186e`.`tbl_category` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
--- src/config/database.js
+++ src/config/database.js
@@ -3,9 +3,9 @@ const mysql = require('mysql');
 const con = mysql.createPool({
          host     : 'us-cdbr-east-05.cleardb.net',
          port     :  3306,
-         user     : 'bb4711e8fb07d5',
-         password : 'c96cbe65',
-         database : 'heroku_ecd2cdd291b0ba0',
+         user     : 'b732077189fa28',
+         password : '14d905c1',
+         database : 'heroku_dc094d6283f186e',
 });
 
 
