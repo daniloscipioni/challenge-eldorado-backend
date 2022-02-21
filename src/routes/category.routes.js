fb83b9abc351b73ec569bc575b16299a0b6b02ff
Danilo Scipioni <daniloedu@gmail.com>
2022-02-20 21:48:45 -0300

    Describe swagger documents

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,13 +1,39 @@
 const router = require('express-promise-router')();
 const categoryController = require('../controllers/category.controller');
 
+/**
+ * GET /api/categories
+ * @summary Returns a list of categories.
+ * @description API that returns a list of categories available to choose.
+ * @tag Categories
+ * @response 200 - A JSON array of user categories
+ * @responseContent {string[]} 200.application/json 
+ */
 router.get('/categories', categoryController.listAllCategories);
 
+/**
+ * POST /api/add-category
+ * @summary Register a new category.
+ * @description API that Register a new category.
+ * @tag Categories
+ * @response 200 - A JSON array of categories
+ * @bodyContent {Categories} application/json
+ * @responseContent {string[]} 200.application/json 
+ */
 router.post('/add-category', async(req, res)=> {
     const register = await categoryController.registerCategory(req.body);
     res.status(201).json(register);
 })
 
+/**
+ * DELETE /api/remove-category/:id
+ * @summary Delete a category.
+ * @description API that deletes a new category.
+ * @tag Categories
+ * @response 200 - A JSON array
+ * @queryParam {integer} [id] - id
+ * @responseContent {string[]} 200.application/json 
+ */
 router.delete('/remove-category/:id', async(req, res)=> {
     const deleteRegister = await categoryController.deleteCategory(req.params.id);
 
--- src/routes/device.routes.js
+++ src/routes/device.routes.js
@@ -1,13 +1,37 @@
 const router = require('express-promise-router')();
 const deviceController = require('../controllers/device.controller');
 
+/**
+ * GET /api/devices
+ * @summary Returns a list of devices.
+ * @description API that returns a list of devices registered.
+ * @tag Devices
+ * @response 200 - A JSON array of devices
+ * @responseContent {string[]} 200.application/json 
+ */
 router.get('/devices', deviceController.listAllDevices);
 
+/**
+ * POST /api/add-device
+ * @summary Register a new device.
+ * @description API that register a new device.
+ * @tag Devices
+ * @response 200 - A JSON array
+ * @responseContent {string[]} 200.application/json 
+ */
 router.post('/add-device', async(req, res)=> {
     const register = await deviceController.registerDevice(req.body);
     res.status(201).json(register);
 })
 
+/**
+ * DELETE /api/remove-device/:id
+ * @summary Delete a device.
+ * @description API that deletes a device.
+ * @tag Devices
+ * @response 200 - A JSON array
+ * @responseContent {string[]} 200.application/json 
+ */
 router.delete('/remove-device/:id', async(req, res)=> {
     const deleteRegister = await deviceController.deleteDevice(req.params.id);
     res.status(200).json(deleteRegister);
