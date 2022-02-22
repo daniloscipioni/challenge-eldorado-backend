da8b3cb6410e89d4aba9b3477b55d938281f0c90
Danilo Scipioni <daniloedu@gmail.com>
2022-02-21 22:12:14 -0300

    Adda tests for posts

--- src/tests/category.test.js
+++ src/tests/category.test.js
@@ -2,10 +2,24 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Get Endpoints', () => {
-  it('should get all categories', async () => {
+  test('should get all categories', async () => {
     const res = await request(app)
       .get('/api/categories');
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('data');
   });
 });
+
+describe('Post Endpoints', () => {
+    test('should create a new categry', async () => {
+        const res = await request(app)
+            .post('/api/add-category')
+            .send({
+                name: 'Test Category',
+            })
+        expect(res.statusCode).toEqual(201)
+        expect(res.body).toHaveProperty('data')
+    })
+})
+
+
--- src/tests/device.test.js
+++ src/tests/device.test.js
@@ -1,11 +1,30 @@
 const request = require('supertest')
 const app = require('../app')
+const axios = require('axios');
 
 describe('Get Endpoints', () => {
-    it('should get all devices', async () => {
+    test('should get all devices', async () => {
         const res = await request(app)
             .get('/api/devices')
         expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('data')
+    })
+})
+
+describe('Post Endpoints', () => {
+    test('should create a new device', async () => {
+        
+        const res = await request(app)
+            .post('/api/add-device')
+            .send({
+                // change for a existing value
+                category: 24,
+                color: 'color test',
+                partNumber: 123
+            })
+
+        // console.log(res);
+        expect(res.statusCode).toEqual(201)
         expect(res.body).toHaveProperty('data')
     })
 })
