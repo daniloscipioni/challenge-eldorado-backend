b3f315661db2a39ab082559d7019255f75d53cc3
Danilo Scipioni <daniloedu@gmail.com>
2022-02-21 22:28:47 -0300

    Simple tests finalized

--- src/tests/category.test.js
+++ src/tests/category.test.js
@@ -2,16 +2,16 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Get Endpoints', () => {
-  test('should get all categories', async () => {
-    const res = await request(app)
-      .get('/api/categories');
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('data');
-  });
+    test('should get all categories', async () => {
+        const res = await request(app)
+            .get('/api/categories');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('data');
+    });
 });
 
 describe('Post Endpoints', () => {
-    test('should create a new categry', async () => {
+    test('should create a new category', async () => {
         const res = await request(app)
             .post('/api/add-category')
             .send({
@@ -22,4 +22,13 @@ describe('Post Endpoints', () => {
     })
 })
 
+describe('Delete Endpoints', () => {
+    test('should delete a category', async () => {
+        const res = await request(app)
+            // change for an existing value
+            .delete('/api/remove-category/114')
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('data')
+    })
+})
 
--- src/tests/device.test.js
+++ src/tests/device.test.js
@@ -13,18 +13,27 @@ describe('Get Endpoints', () => {
 
 describe('Post Endpoints', () => {
     test('should create a new device', async () => {
-        
+
         const res = await request(app)
             .post('/api/add-device')
             .send({
-                // change for a existing value
-                category: 24,
+                // change for an existing value
+                category: 34,
                 color: 'color test',
                 partNumber: 123
             })
 
-        // console.log(res);
         expect(res.statusCode).toEqual(201)
         expect(res.body).toHaveProperty('data')
+    })
+})
+
+describe('Delete Endpoints', () => {
+    test('should delete a device', async () => {
+        const res = await request(app)
+            // change for an existing value
+            .delete('/api/remove-device/34')
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).not.toEqual('Device must be removed')
     })
 })
