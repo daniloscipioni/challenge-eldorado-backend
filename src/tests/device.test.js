6e43fcd4b1d2b716df9afb2bdb92470b62887ae4
Danilo Scipioni <daniloedu@gmail.com>
2022-02-21 21:12:22 -0300

    Configure category and device for test

--- src/tests/device.test.js
+++ src/tests/device.test.js
@@ -1,26 +1,11 @@
 const request = require('supertest')
 const app = require('../app')
 
-const url = 'https://challenge-eldorado-backend.herokuapp.com';
-
-describe('Post Endpoints', () => {
-    it('should create a new post', async () => {
+describe('Get Endpoints', () => {
+    it('should get all devices', async () => {
         const res = await request(app)
-            .get('/api/categories')
+            .get('/api/devices')
         expect(res.statusCode).toEqual(200)
-        expect(res.body).toHaveProperty('data')
-    })
-})
-
-
-describe('Post Endpoints', () => {
-    it('should create a new post', async () => {
-        const res = await request(app)
-            .post('/api/add-category')
-            .send({
-                name: 'test is cool',
-            })
-        expect(res.statusCode).toEqual(201)
         expect(res.body).toHaveProperty('data')
     })
 })
